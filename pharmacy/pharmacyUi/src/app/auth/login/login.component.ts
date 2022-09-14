c35ba5224f865010b6c8414279d761e58b5acddc
dawnregi <dawnregipanthalany@gmail.com>
2022-09-14 14:36:44 +0530

    completed login front end

--- pharmacy/pharmacyUi/src/app/auth/login/login.component.ts
+++ pharmacy/pharmacyUi/src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/common-lib/service/api.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: ApiService) { }
 
   ngOnInit(): void {
+  }
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)])
+  })
+
+
+  login() {
+    if (this.loginForm.valid ) {
+      this.service.loginUser(this.loginForm.value).subscribe({
+        next:(res:any)=>{
+          alert("login Success"),
+          console.log(res);
+          
+        }
+      })
+    }
+    else{
+      alert("Enter a valid form")
+    }
   }
 
 }
