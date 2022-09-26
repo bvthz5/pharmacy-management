93b1f573186702416aae6b40364d8a9061e4f9c5
devnandans <devndn8900@gmail.com>
2022-09-26 14:03:02 +0530

    *****

--- pharmacy/pharmacyUi/src/app/auth/saleshistory/saleshistory.component.ts
+++ pharmacy/pharmacyUi/src/app/auth/saleshistory/saleshistory.component.ts
@@ -125,7 +125,7 @@ export class SaleshistoryComponent implements OnInit {
         this.service.filter(this.addForm.value.days).subscribe(res => {
 
           this.salesdata = res
-          alert("success")
+          // alert("success")
           } );
         // this.router.navigate(['itemlist'])
         
